8767c1cc0ba088db1425c0e8d3ac983e06494b85
Jean-Luck Sithi <jean-lucksithi@MacBook-Air-de-Jean-Luck.local>
2025-04-23 14:00:05 +0200

    petite correction

--- api/permissions.py
+++ api/permissions.py
@@ -9,9 +9,4 @@ class IsCreateurOrReadOnly(permissions.BasePermission):
 
         return obj.createur_id == request.user.id
     
-    def has_permission(self, request, view):
-        if request.data.get('isbn'):
-            livre = Livre.objects.get(isbn=request.data.get('isbn'))
-            return livre.createur_id == request.user.id
-        return super().has_permission(request, view)
                 
