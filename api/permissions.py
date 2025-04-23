a9a64ba51ed9d7e5324c68edcc7af7c54912cf82
Jean-Luck Sithi <jean-lucksithi@MacBook-Air-de-Jean-Luck.local>
2025-04-23 15:53:49 +0200

    fonction inutile

--- api/permissions.py
+++ api/permissions.py
@@ -8,10 +8,4 @@ class IsCreateurOrReadOnly(permissions.BasePermission):
             return True
 
         return obj.createur_id == request.user.id
-    
-    def has_permission(self, request, view):
-        if request.data.get('isbn'):
-            livre = Livre.objects.get(isbn=request.data.get('isbn'))
-            return livre.createur_id == request.user.id
-        return super().has_permission(request, view)
                 
